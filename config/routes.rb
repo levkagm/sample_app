1d334b6d1b94876bd780f74d6860ecd62270a091
levkagm <levkagm@gmail.com>
2015-10-01 18:50:07 +0300

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root 'static_pages#home'
-  get 'help'    => 'static_pages#help',     as: :help
-  get 'about'   => 'static_pages#about',    as: :about
-  get 'contact' => 'static_pages#contact',  as: :contact
-  get 'signup'  => 'users#new'
+  get 'help'       => 'static_pages#help',     as: :help
+  get 'about'      => 'static_pages#about',    as: :about
+  get 'contact'    => 'static_pages#contact',  as: :contact
+  get 'signup'     => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   resources :users
 
 
